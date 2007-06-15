fcf0aaee2f3d01cc14e8015f9a4ea6962bc9f24c
denverg <Unknown>
2007-06-15 20:21:27 +0000

    wdiff 0.5.1 - changes from wdiff 0.5  - updated license notices to reflect new FSF address  - added license notices to files that did not have them

---
+++ acconfig.h
@@ -0,0 +1,36 @@
+/* Special definitions for wdiff, processed by autoheader.
+   Copyright (C) 1994 Free Software Foundation, Inc.
+   Francois Pinard <pinard@iro.umontreal.ca>, 1994.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Define to the path of the `diff' utility.  */
+#undef DIFF_PROGRAM
+
+/* Define to the path of the `less' or `more' utility.  */
+#undef PAGER_PROGRAM
+
+/* Define to the name of the distribution.  */
+#undef PRODUCT
+
+/* Define to 1 if ANSI function prototypes are usable.  */
+#undef PROTOTYPES
+
+/* Define to the version of the distribution.  */
+#undef VERSION
+
+/* Define to 1 for better use of the debugging malloc library.  See 
+   site ftp.antaire.com in antaire/src, file dmalloc/dmalloc.tar.gz.  */
+#undef WITH_DMALLOC
---
+++ writepipe.c
@@ -0,0 +1,92 @@
+/* Open a pipe to write to a program without intermediary sh.
+   Copyright (C) 1992 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Written by David MacKenzie.  */
+
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
+
+#if __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+#if HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+/* Open a pipe to write to a program without intermediary sh.  Checks
+   PATH.  Sample use:
+   
+   stream = writepipe ("progname", "arg1", "arg2", (char *) 0);
+   
+   Return 0 on error.  */
+
+#if __STDC__
+FILE *
+writepipe (char *progname, ...)
+#else
+FILE *
+writepipe (...)
+#endif
+{
+#if ! __STDC__
+  char *progname;
+#endif
+  int fds[2];
+  va_list ap;
+  char *args[100];
+  int argno = 0;
+
+  /* Copy arguments into `args'. */
+#if __STDC__
+  va_start (ap, progname);
+#else
+  va_start (ap);
+  progname = va_arg (ap, char *);
+#endif
+  args[argno++] = progname;
+  while ((args[argno++] = va_arg (ap, char *)) != NULL)
+    ;
+  va_end (ap);
+
+  if (pipe (fds) == -1)
+    return 0;
+
+  switch (fork ())
+    {
+    case 0:			/* Child.  Read from pipe. */
+      close (fds[1]);		/* Not needed. */
+      if (fds[0] != 0)		/* Redirect 0 (stdin) only if needed.  */
+	{
+	  close (0);		/* We don't want the old stdin. */
+	  dup (fds[0]);		/* Guaranteed to dup to 0 (stdin). */
+	  close (fds[0]);	/* No longer needed. */
+	}
+      execvp (args[0], args);
+      _exit (2);		/* 2 for `cmp'. */
+    case -1:			/* Error. */
+      return 0;
+    default:			/* Parent.  Write to pipe. */
+      close (fds[0]);		/* Not needed. */
+      return fdopen (fds[1], "w");
+    }
+}
